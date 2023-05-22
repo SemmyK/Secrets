0ad9b5a539cc91ed7bcd3be6c6c22983c5eae19f
SemmyK <66922979+SemmyK@users.noreply.github.com>
2023-05-22 19:03:34 +0200

    Level 6 security implemented Google Oauth

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ require('dotenv').config()
 const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 const session = require('express-session')
+//mongoose
+const mongoose = require('mongoose')
+//passport
 const passport = require('passport')
 const passportLocalMongoose = require('passport-local-mongoose')
-//const LocalStrategy = require('passport-local')
-
+const LocalStrategy = require('passport-local').Strategy
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const PORT = process.env.PORT || 3000
 
-//create express app
+//create and configure express app
 const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -44,9 +46,17 @@ const connectDB = async () => {
 
 //create a SCHEMA that sets out the fields each document will have and their datatypes
 const userSchema = new mongoose.Schema({
+	googleId: {
+		type: String,
+		required: false,
+	},
 	email: {
 		type: String,
-		required: [true, 'No name specified!'],
+		required: false,
+	},
+	name: {
+		type: String,
+		required: false,
 	},
 	password: {
 		type: String,
@@ -64,11 +74,54 @@ userSchema.plugin(passportLocalMongoose, {
 //create model from schema
 const User = new mongoose.model('User', userSchema)
 
-//create session for user
+// Configure Passport to use the LocalStrategy for local authentication
+passport.use(new LocalStrategy(User.authenticate()))
+// Configure Passport to use the GoogleStrategy for Google OAuth
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: 'http://localhost:3000/auth/google/secrets',
+			state: true,
+		},
+		async (accessToken, refreshToken, profile, done) => {
+			try {
+				// Find or create the user in your database
+				const user = await User.findOne({
+					googleId: profile.id,
+					name: profile.displayName,
+				})
 
-passport.use(User.createStrategy())
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
+				if (user) {
+					done(null, user)
+				} else {
+					const newUser = await User.create({
+						googleId: profile.id,
+						name: profile.displayName,
+					})
+					done(null, newUser)
+				}
+			} catch (error) {
+				done(error, null)
+			}
+		}
+	)
+)
+
+// Serialize and deserialize user instances to and from the session
+passport.serializeUser(function (user, done) {
+	done(null, user.id)
+})
+passport.deserializeUser(async (id, done) => {
+	try {
+		const user = await User.findById(id)
+
+		done(null, user)
+	} catch (error) {
+		done(error, null)
+	}
+})
 
 //requests to db and server
 
@@ -79,6 +132,18 @@ app.route('/').get((req, res) => {
 		console.log('There was an error.')
 	}
 })
+
+// Google OAuth route
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }))
+
+// Google OAuth callback route
+app.get(
+	'/auth/google/secrets',
+	passport.authenticate('google', { failureRedirect: '/login' }),
+	(req, res) => {
+		res.redirect('/secrets') // Redirect to the home page after successful authentication
+	}
+)
 
 app
 	.route('/login')
@@ -99,23 +164,35 @@ app
 				password: password,
 			})
 
-			User.authenticate('local')(req, res, () => {
-				req.logIn(alreadyUser, err => {
-					if (err) {
-						console.log(err)
+			//check if user already registered
+			//find user that matches in db
+			User.findOne({ email: alreadyUser.email })
+				.then(foundUser => {
+					if (!foundUser) {
+						console.log('No user found. User=' + username)
+						res.redirect('/register')
+					} else {
+						passport.authenticate('local', {
+							successRedirect: '/secrets',
+							failureRedirect: '/login',
+						})
+						User.authenticate('local')(req, res, () => {
+							req.logIn(foundUser, err => {
+								if (err) {
+									console.log(err)
+								}
+								if (req.isAuthenticated()) {
+									res.redirect('/secrets')
+								} else {
+									console.log('Error in authentication.')
+								}
+							})
+						})
 					}
-					res.redirect('/secrets')
 				})
-			})
-
-			// req.logIn(alreadyUser, function (err) {
-			// 	if (err) {
-			// 		throw err
-			// 	} else {
-			// 		res.redirect('/secrets')
-			// 	}
-			// 	// session saved
-			// })
+				.catch(err => {
+					console.log(err)
+				})
 		} else {
 			console.log('There was an error while loging in user.')
 		}
@@ -153,24 +230,40 @@ app
 		if (res.statusCode === 200) {
 			const username = req.body.username
 			const password = req.body.password
+
 			const newUser = new User({
 				email: username,
 			})
-			User.register(newUser, password, (err, user) => {
-				if (err) {
-					console.log(err)
-					res.redirect('/register')
-				} else {
-					User.authenticate('local')(req, res, () => {
-						req.logIn(user, err => {
+
+			//check if user already registered
+			//find user that matches in db
+			User.findOne({ email: username })
+				.then(foundUser => {
+					if (!foundUser) {
+						console.log('No user found. User=' + username)
+						User.register(newUser, password, (err, user) => {
 							if (err) {
 								console.log(err)
+								res.redirect('/register')
+							} else {
+								User.authenticate('local')(req, res, () => {
+									req.logIn(user, err => {
+										if (err) {
+											console.log(err)
+										}
+										res.redirect('/secrets')
+									})
+								})
 							}
-							res.redirect('/secrets')
 						})
-					})
-				}
-			})
+					} else {
+						console.log('User already registered')
+						res.redirect('/login')
+					}
+				})
+				.catch(err => {
+					console.log(err)
+				})
 		} else {
 			console.log('There was an error while registering user.')
 		}
@@ -182,29 +275,3 @@ connectDB().then(() => {
 		console.log('listening for requests')
 	})
 })
-
-// //find user that matches in db
-// User.findOne({ email: username })
-// 	.then(foundUser => {
-// 		if (!foundUser) {
-// 			console.log('No user found. User=' + username)
-// 		} else {
-// 			//check the password
-// 			// Load hash from your password DB.
-// 			bcrypt
-// 				.compare(password, foundUser.password)
-// 				.then(function (result) {
-// 					if (result === true) {
-// 						res.render('secrets')
-// 					} else {
-// 						console.log('User not found in Database.')
-// 					}
-// 				})
-// 				.catch(err => {
-// 					console.log(err)
-// 				})
-// 		}
-// 	})
-// 	.catch(err => {
-// 		console.log(err)
-// 	})
